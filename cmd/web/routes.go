8b61d81a7cbfeb2ec36db88aee85bcaec8249004
pedro-git-projects <pedro.generic.email@gmail.com>
2022-02-14 22:28:58 -0300

    initial setup

---
+++ cmd/web/routes.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+	return mux
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/pedro-git-projects/e-commerce-go
+
+go 1.17
+
+require github.com/go-chi/chi v1.5.4
